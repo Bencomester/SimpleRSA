ae726a1d67cc0c97c2bd557dc33db03943a462da
Bence László Szilágyi <bencomester@gmail.com>
2026-05-18 10:40:10 +0200

    Updated Main.java

--- src/main/java/rsa/Main.java
+++ src/main/java/rsa/Main.java
@@ -1,5 +1,6 @@
 package rsa;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class Main {
@@ -12,9 +13,16 @@ public class Main {
         System.out.println(pk);
         System.out.println(sk);
 
-        int m = 356;
-        int c = rsa.encrypt(m, pk);
-        System.out.println(c);
-        System.out.println(rsa.decrypt(c, sk));
+        int m1 = 356;
+        String m2 = "Message to encrypt";
+
+        int c1 = rsa.encrypt(m1, pk);
+        int[] c2 = rsa.encrypt(m2, pk);
+
+        System.out.println(c1);
+        System.out.println(rsa.decrypt(c1, sk));
+
+        System.out.println(Arrays.toString(c2));
+        System.out.println(rsa.decrypt(c2, sk));
     }
 }
